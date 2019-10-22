b87f3ea362d6254f2a43b31770ef490e231a63ab
Mike <michael005@me.com>
2019-10-22 01:06:35 -0400

    First Commit Lab 7

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CapStoneAssignment
+{
+    class Program
+    {
+        public static char [] vowels = "aeiou".ToCharArray(); // universal array so we can use in any method// 
+        static void Main(string[] args)
+        {
+            bool answer = true;
+            while (answer == true)
+            {
+                Console.WriteLine("Please enter a word...");         
+             string word = Console.ReadLine();
+
+                foreach (char c in vowels) // for(int i = 0: i < vowels.Length; i++//
+                {
+                    if (word.StartsWith(c)) //if(word.StartsWith(c)) vowel[i])))
+                    {
+                        Console.WriteLine(StartsWithVowel(word));
+                        
+                    }
+                }
+               
+
+                    DoesNotStartWithaVowels(word);
+                    
+
+                    Console.WriteLine("Do you want to go again");
+                    answer = TranslateAnotherOne(Console.ReadLine());
+                
+
+            }
+        }
+        public static void DoesNotStartWithaVowels(string word)
+        {
+            word = word.ToLower();
+            int index = word.IndexOfAny(vowels);
+            string firstPart = word.Substring(index);
+            string secondPart = word.Substring(3, index);
+
+            
+                {
+
+                    Console.WriteLine($"{firstPart}{secondPart}ay");
+                    
+                
+                }
+            
+        }
+        
+      public static string StartsWithVowel (string word)
+        {
+            for (int i = 0; i < vowels.Length; i++)
+            {
+                word.ToLower();
+                if (word.StartsWith(vowels[i]))
+                {
+                    return $"{word}way";
+                }
+            }
+            return word + "way";
+        }   
+        public static bool TranslateAnotherOne(string response)
+        {
+            if (response == "y" || response == "yes")
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+}
